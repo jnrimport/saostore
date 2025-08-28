b778ce8d5af459a98ef26409136d3a71a22716b6
jnrimport <admboutiqueanna@gmail.com>
2025-08-28 13:35:50 -0300

    Delete js directory

--- js/custom.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){var buyNowButton=document.getElementById("BuyNowButton");buyNowButton&&buyNowButton.addEventListener("click",function(){window.location.href="/checkout"})});
-//# sourceMappingURL=/cdn/shop/t/10/assets/custom.js.map?v=49488239296919326971755480407
